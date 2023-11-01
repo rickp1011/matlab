5f18447cfd382c1728bc420a0f49f004884a4280
rickp1011 <92723258+rickp1011@users.noreply.github.com>
2023-11-01 23:03:13 +0800

    commented

--- method 1/fourvariable.m
+++ method 1/fourvariable.m
@@ -4,6 +4,7 @@ Function to count columns with four criteria x is the input array , a is the cou
 col is the column we want 
 h,j,k,l is the criteria stated in the problem like for example in col 12 where it is to represent the number of blood vessesls coloured 
 h means  0 blood vessel coloured , j is 1 blood vessel coloured and k is 2 blood vessel coloured , and l is 3 blood vessel coloured
+functions that use fourvariable function are : 3 and 12 
 %
     for t=1:size(x)
         switch(x(t,col))
