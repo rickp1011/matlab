6a378a8172cf9c03c6594231518e7f34c7a0bd98
rickp1011 <92723258+rickp1011@users.noreply.github.com>
2023-11-01 23:10:38 +0800

    comments

--- method 1/positive.m
+++ method 1/positive.m
@@ -1,4 +1,7 @@
 function [z,q,t] = positive()
+%
+this function is to sort the Heart attack and the healthy  into different arrays  of heart attack and healthy 
+%
     load("HeartDisease.mat");
     x=sortrows(T,14,"descend");
     for t = 1:size(x)
@@ -8,9 +11,12 @@ function [z,q,t] = positive()
             q(t,:)= x(t,:);
         end
     end
+    % sorting of the different arrays those that have nan in the last row will not be counted
     indicesz = z(:,1) == 0;
     z(indicesz,:) = [];
     indicesq = q(:,1) == 0;
     q(indicesq,:) = []; 
     t = length(T);
+    %removing zeros for those that are zeros so that it wont be counted in the array
+    total 48 healty 39 heart attack
 end
