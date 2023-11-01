24b242808309839232936b3bfad934781d949c62
jovyyz <148076752+jovyyz@users.noreply.github.com>
2023-11-01 23:02:41 +0800

    Update probablity.m

--- method 1/probablity.m
+++ method 1/probablity.m
@@ -1,6 +1,8 @@
-    function o = probablity(o,T)
-        prob_female = o(2) /T *100;
-        prob_male = o(1) /T*100;
-        o = [prob_female,prob_male];
+    function o = probablity(o,T) % o is a 2 element array representing counts of females and males
+    % T is the total count of females and males
+        prob_female = o(2) /T *100;  % divide the count of females o(2) by the total count (T)
+    % multiply by 100 to express as a percentage
+        prob_male = o(1) /T*100; % divide the count of males 0(1) by the total count (T)
+    % multiply by 100 to express as a percentage   
+        o = [prob_female,prob_male]; % o array containing calculated probabilities of females and males
     end
-        %function to calculate the probabilty
