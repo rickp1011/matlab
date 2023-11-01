cf43c7e54e2329a88cc51d5303ec84bad3711de1
rickp1011 <92723258+rickp1011@users.noreply.github.com>
2023-11-01 23:02:22 +0800

    comments

--- method 1/threevaraible.m
+++ method 1/threevaraible.m
@@ -1,4 +1,11 @@
 function z = threevaraible(x,a,col,j,k,l)
+%
+Function to count columns with three criteria x is the input array , a is the counts put in an array
+col is the column we want 
+j,k,l is the criteria stated in the problem like for example in col 13  where it is to represent the thalium stress test result
+j is 3 , k is 6 and l is 7 
+columns that use three variable function are :7, 11 and 13
+%
     for t=1:size(x)
         switch(x(t,col))
             case j
